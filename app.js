18612c52b58b3c2122dd08dd46cebbc3380e34db
Judith Mueni <judithmueni395@gmail.com>
2018-05-08 12:17:57 +0300

    Vue framework

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-const form = document.querySelector('form')
-const taskInput = document.querySelector('input[name = taskName]')
-const dateInput = document.querySelector('input[name = taskDate]')
-const ul = document.querySelector('ul')
-
-form.onsubmit = function(e) {
-	e.preventDefault()
-	
-	let taskTitle = taskInput.value
-	let taskDate = dateInput.value
-
-	//create the list element
-	const li = document.createElement('li')
-	li.textContent = taskTitle
-	li.classList.add('list-group-item')
-	li.classList.add('d-flex')
-	li.classList.add('justify-content-between')
-	li.classList.add('align-items-center')
-
-
-
-	const span = document.createElement('span')
-	span.textContent = taskDate
-	span.classList.add('badge')
-	span.classList.add('badge-primary')
-	span.classList.add('badge-pill')
-
-	li.appendChild(span)
-	ul.appendChild(li)
-}
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [	
+    	"My first to do item",
+    	"My second to do item",
+    	"My third to do item"
+    ],
+    item: 'To do item'
+  },
+  methods: {
+  	addTodo() {
+  		this.todos.push(this.item)
+  		this.item = '';
+  	}
+  }
+})
